25d2feebc439bf1dde364296c31d2e449968fabc
DChang87 <diana.chang.twn@gmail.com>
2015-05-06 21:36:46 -0400

    MainCharacter 05/06/2015

---
+++ MainCharacter.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class MainCharacter {
+	private int health;
+	private String name;
+	private int posx,posy;
+	private final int NUMOFWEAPONS = 5;
+	private int[] weapons = new int[NUMOFWEAPONS];
+	public MainCharacter(String n){
+		name = n;
+	}
+	public int getX(){
+		return posx;
+	}
+	public int getY(){
+		return posy;
+	}
+	public void setX(int x){
+		posx = x;
+	}
+	public void setY(int y){
+		posy = y;
+	}
+	public void setHealth(int h){
+		health=h;
+	}
+	public int getHealth(){
+		return health;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setName(String n){
+		name = n;
+	}
+	
+}
