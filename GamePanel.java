d7de3a42d8936e09160d7daf45e67c6ad05a4b79
Leo F. <leofeng1999@gmail.com>
2015-05-20 19:09:02 -0400

    GamePanel Leo 05/20/15 7:08

--- GamePanel.java
+++ GamePanel.java
@@ -38,9 +38,7 @@ public class GamePanel extends JPanel implements KeyListener{
 		return (fx>=mcx && fx <=mcx+BH.mc.getWidth() && fy>=mcy && fy<=mcy+BH.mc.getLength());
 	}
 	public void MCshoot(){
-		System.out.println("MCshoot");
 		if (keys[KeyEvent.VK_SPACE]){
-			System.out.println("SPACE");
 			//if the user shoots, add a bullet into the arraylist keeping track of flying bullets
 			//BH.addBullet(new PosPair(BH.mc.getX(),BH.mc.getY(),BH.mc.getANGLE(),BH.getWeapon()));
 			BH.activeBullets.add(new PosPair(BH.mc.getX(),BH.mc.getY(),BH.mc.getANGLE(),BH.mc.getWeapon()));
@@ -49,17 +47,38 @@ public class GamePanel extends JPanel implements KeyListener{
 	public void moveMC(){
 		if (keys[KeyEvent.VK_LEFT]){
 			//maybe set the direction lol
+			BH.mc.setAngle(180);
 			BH.mc.setX(Math.max(0, BH.mc.getX()-5));
 		}
 		else if (keys[KeyEvent.VK_RIGHT]){
 			BH.mc.setX(Math.min(800-BH.mc.getWidth(), BH.mc.getX()+5));
+			BH.mc.setAngle(0);
 		}
 		else if (keys[KeyEvent.VK_UP]){
 			BH.mc.setY(Math.max(0, BH.mc.getY()-5));
+			BH.mc.setAngle(270);
 		}
 		else if (keys[KeyEvent.VK_DOWN]){
 			BH.mc.setY(Math.min(640-BH.mc.getLength(),BH.mc.getY()+5));
+			BH.mc.setAngle(90);
 		}
+	}
+	public boolean enemyCollision(int x, int y){
+		ArrayList<Zombie> toRemove = new ArrayList<Zombie>();
+		boolean flag = false;
+		for (int i=0; i<BH.allZombies.size(); i++){
+			if (BH.allZombies.get(i).getCollide(x,y)){
+				flag = true;
+				BH.allZombies.get(i).setHealth(BH.allZombies.get(i).getHealth() - 10);
+			}
+			if (BH.allZombies.get(i).getHealth() <= 0){
+				toRemove.add(BH.allZombies.get(i));
+			}
+		}
+		for (Zombie zzh8829:toRemove){
+			BH.allZombies.remove(zzh8829);
+		}
+		return flag;
 	}
 	public int calculateHealth(){
 		//full health is at 100
@@ -71,8 +90,7 @@ public class GamePanel extends JPanel implements KeyListener{
 	public void checkBulletCollision(){
 		ArrayList<PosPair> toRemove = new ArrayList<PosPair>();
 		for (int i=0;i<BH.activeBullets.size();i++){
-			if (checkCollision(BH.activeBullets.get(i).getX(),BH.activeBullets.get(i).getY(),BH.mc.getX(),BH.mc.getY())){
-				BH.mc.setHealth(BH.mc.getHealth()-10); //change this to the actual value later;
+			if (enemyCollision(BH.activeBullets.get(i).getX(),BH.activeBullets.get(i).getY())){
 				toRemove.add(BH.activeBullets.get(i));
 			}
 			else if (checkOutside(BH.activeBullets.get(i).getX(),BH.activeBullets.get(i).getY())){
@@ -88,13 +106,14 @@ public class GamePanel extends JPanel implements KeyListener{
 		for (int i=0;i<BH.activeBullets.size();i++)
 		{
 			PosPair temp = BH.activeBullets.get(i);
-			final int ANG = temp.getANGLE();
+			final double ANG = Math.toRadians(temp.getANGLE());
 			double xx = temp.getDX(), yy = temp.getDY();
 			BH.activeBullets.get(i).setPos(xx+10*Math.cos(ANG),yy+10*Math.sin(ANG));
 		}
 	}
 	public void moveFireballs()
 	{
+		ArrayList<PosPair> toRemove = new ArrayList<PosPair>();
 		for (int i=0;i<BH.fireballs.size();i++)
 		{
 			PosPair temp = BH.fireballs.get(i);
@@ -104,7 +123,14 @@ public class GamePanel extends JPanel implements KeyListener{
 			if (checkCollision(BH.fireballs.get(i).getX(),BH.fireballs.get(i).getY(),BH.mc.getX(),BH.mc.getY())){
 				//decrease health by 15 points per fireball
 				BH.mc.setHealth(BH.mc.getHealth()-15);
+				toRemove.add(temp);
 			}
+			else if (checkOutside(BH.fireballs.get(i).getX(),BH.fireballs.get(i).getY())){
+				toRemove.add(temp);
+			}
+		}
+		for (PosPair pair:toRemove){
+			BH.fireballs.remove(pair);
 		}
 	}
 	public void paintComponent(Graphics g){
@@ -126,7 +152,12 @@ public class GamePanel extends JPanel implements KeyListener{
 		for (int i=0;i<BH.fireballs.size();i++){
 			g.drawOval(BH.fireballs.get(i).getX(), BH.fireballs.get(i).getY(), 50, 50);
 		}
+		for (int i=0;i<BH.allZombies.size();i++){
+			Zombie a = BH.allZombies.get(i);
+			g.drawRect(a.getX(),a.getY(),a.getsx(), a.getsy());
+		}
+		
 		g.setColor(new Color (255,0,0));
-		g.drawRect(BH.mc.getX(),BH.mc.getY(),30,70);
+		g.drawRect(BH.mc.getX(),BH.mc.getY(),BH.mc.getsx(),BH.mc.getsy());
 	}
 }
