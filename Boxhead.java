e7eb93e0abfa8c506f3aeb9246dcab2e9870da2e
DChang87 <diana.chang.twn@gmail.com>
2015-06-02 11:24:51 -0400

    Boxhead 06/02/2015 11:@4am

--- Boxhead.java
+++ Boxhead.java
@@ -8,7 +8,7 @@ import javax.swing.Timer;
 public class BoxHead extends JFrame implements ActionListener{
 	Timer fireTimer,myTimer,shootTimer;
 	public final int START=0,GAME = 1,PAUSE=2;
-	public int state=GAME;
+	public int state=START;
 	public int score = 0;
 	GamePanel game;
 	public ArrayList<Zombie> allZombies = new ArrayList<Zombie>(); //this stores all of the zombies that are currently in the game
@@ -19,12 +19,15 @@ public class BoxHead extends JFrame implements ActionListener{
 	public ArrayList<Image> bulletSprites=new ArrayList<Image>();
 	MainCharacter mc;
 	StartScreen startS;
+	//add this
+	SelectionMenu sm;
+	//clos add this
 	public BoxHead(){
 		super("BoxHead Zombies");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800,640);
 		setLayout(new BorderLayout());
-		game = new GamePanel(this);
+		
 		shootTimer = new Timer(200,this);
 		fireTimer = new Timer(100,this); //timer used to increase the timeCount on the fireball for the devil
 		myTimer = new Timer(20,this); //myTimer is used to record the time for general movements in the game
@@ -34,9 +37,16 @@ public class BoxHead extends JFrame implements ActionListener{
 		startS.setLocation(0,0);
 		startS.setSize(800,640);
 		add(startS);
+		game = new GamePanel(this);
 		game.setLocation(0,0);
 		game.setSize(800,640);
 		add(game);
+		//add this
+		sm = new SelectionMenu(this);
+		sm.setLocation(0,0);
+		sm.setSize(800,640);
+		add(sm);
+		//close add this
 		//game.setFocusable(true);
 		//game.setVisible(true);
 		setResizable(false);
@@ -79,9 +89,11 @@ public class BoxHead extends JFrame implements ActionListener{
 			game.repaint();
 		}
 		else if (state==PAUSE){
+			System.out.println("PAUSE");
 			if (source==myTimer){
-				game.checkUnPause();
+				sm.checkUnPause();
 			}
+			sm.repaint();
 		}
 	}
 	public static void main(String[] args){
