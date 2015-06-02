8c9aec46382281ed0b622b0edb70d88df6f1f1db
DChang87 <diana.chang.twn@gmail.com>
2015-06-02 11:33:57 -0400

    Create SelectionMenu.java

---
+++ SelectionMenu.java
@@ -0,0 +1,86 @@
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+public class SelectionMenu extends JPanel implements MouseMotionListener, MouseListener,KeyListener{
+	private static int mouseX,mouseY;
+	private Image background = new ImageIcon("selectionBackground.jpg").getImage();
+	private boolean down=false;
+	BoxHead BH;
+	private boolean[] keys;
+	public SelectionMenu(BoxHead b){
+		//load the images for the start button
+		keys = new boolean[65535];
+		BH=b;
+		addMouseMotionListener(this);
+		addMouseListener(this);
+		addKeyListener(this);
+		setSize(800,640);
+	}
+    public void addNotify() {
+    	super.addNotify();
+    	requestFocus();
+    }
+    // ------------ MouseListener ------------------------------------------
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {
+    	down=false;
+    }    
+    public void mouseClicked(MouseEvent e){
+    }  
+    	 
+    public void mousePressed(MouseEvent e){
+		down=true;
+	}
+    	
+    // ---------- MouseMotionListener ------------------------------------------
+    public void mouseDragged(MouseEvent e){}
+    public void mouseMoved(MouseEvent e){
+    	mouseX=e.getX();
+    	mouseY = e.getY();
+    }
+    /*
+    public static boolean collide(int x,int y,int width, int height){
+    	//check if the mouse is colliding with the button
+    	return x<=mouseX && mouseX<=x+width && y<=mouseY && mouseY<=y+height;
+    }
+    */
+    public void keyTyped(KeyEvent e){
+		
+	}
+    public void keyPressed(KeyEvent e){
+    	keys[e.getKeyCode()]=true;
+    }
+    public void keyReleased(KeyEvent e){
+    	keys[e.getKeyCode()]=false;
+    }
+    public void checkUnPause(){
+    	//System.out.println("checkunpause");
+		if (keys[KeyEvent.VK_P] || down){
+			BH.state=BH.GAME;
+			setFocusable(false);
+    		BH.game.requestFocus();
+		}
+	}
+    public void paintComponent(Graphics g){
+    	//draw the background and the button (According to the situation)
+    	System.out.println("selection");
+    	g.drawImage(background,0,0,this);
+    	if (down || keys[KeyEvent.VK_P]){
+    		//System.out.println("down selection menu");
+    		BH.state=BH.GAME;
+    		setFocusable(false);
+    		BH.game.requestFocus();
+    		
+    	}
+    }
+    
+	    
+}
