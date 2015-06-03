217dd5c534203205e3cd358e23b98ee363a8b853
DChang87 <diana.chang.twn@gmail.com>
2015-06-02 20:54:48 -0400

    MagicalBox 06/02/2015 8:54pm

--- MagicalBox.java
+++ MagicalBox.java
@@ -1,19 +1,20 @@
 
 class MagicalBox {
 	int ITEM;
-	final int HEALTH=0,PISTOL=1,UZI=2,PISTOLP=11,SHOTGUN=3,UZIP=21,BARREL = 4,UZIPP=22,GRENADE= 5, FAKEWALLS=6; 
+	 
 	//PistolP = Pistol plus, double damage, 1.1
 	//UZIP = UZI rapid fire, 2.1
 	//UZIPP = UZI double ammo, 2.2
-	int count=0;
+	int allowance=1;//allowance for the current state (as the user kills more enemies, the allowance increases and increases the diversity
+	//of the items in the box
 	int X,Y;
 	public MagicalBox(int x, int y){
-		generateItem();
 		X=x;
 		Y=y;
 	}
-	public void generateItem(){
-		ITEM=count;
+	public int generateItem(){
+		ITEM=(int) (Math.random()*allowance);
+		return ITEM;
 	}
 	public int getX(){
 		return X;
