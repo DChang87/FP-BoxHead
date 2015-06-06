a1a8eb23f56480087133880fd3a38426176eb27d
Diana Chang <diana.chang.twn@gmail.com>
2015-06-06 11:04:32 -0400

    StartScreen 06/06/2015 11:04ma
    startS button semi fixed

--- StartScreen.java
+++ StartScreen.java
@@ -6,7 +6,7 @@ import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
-public class StartScreen extends JPanel implements MouseMotionListener, MouseListener{
+public class StartScreen extends JPanel{
 	private static int mouseX,mouseY;
 	private Image background = new ImageIcon("tempbackground.jpg").getImage();
 	private boolean down=false;
@@ -14,63 +14,15 @@ public class StartScreen extends JPanel implements MouseMotionListener, MouseLis
 	private int counter=50;
 	public StartScreen(BoxHead b){
 		//load the images for the start button
-		System.out.println("public startscreen");
 		BH=b;
-		addMouseMotionListener(this);
-		addMouseListener(this);
 		setSize(800,640);
 	}
-    public void addNotify() {
-    	super.addNotify();
-    	requestFocus();
-    }
-    // ------------ MouseListener ------------------------------------------
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
-    public void mouseReleased(MouseEvent e) {
-    	down=false;
-    }    
-    public void mouseClicked(MouseEvent e){
-    }  
-    	 
-    public void mousePressed(MouseEvent e){
-		System.out.println("mousepressed");
-    	down=true;
-	}
-    	
-    // ---------- MouseMotionListener ------------------------------------------
-    public void mouseDragged(MouseEvent e){}
-    public void mouseMoved(MouseEvent e){
-    	mouseX=e.getX();
-    	mouseY = e.getY();
-    }
-    
-    public static boolean collide(int x,int y,int width, int height){
-    	//check if the mouse is colliding with the button
-    	return x<=mouseX && mouseX<=x+width && y<=mouseY && mouseY<=y+height;
-    }
-    
-    public void checkDown(){
-    	if (down){
-    		System.out.println("start screen down");
-    		BH.state=BH.GAME;
-    		//setFocusable(false);
-    		BH.game.requestFocus();
-    	}
-    }
+    //public void addNotify() {
+    	//super.addNotify();
+    	//requestFocus();
+    //}
     public void paintComponent(Graphics g){
-    	//draw the background and the button (According to the situation)
     	g.drawImage(background,0,0,this);
-    	//System.out.println("Start screen paint component");
-    	checkDown();
-    	counter--;
-    	System.out.println(counter+" counter");
-    	if (counter==0){
-    		System.out.println("start screen down");
-    		BH.state=BH.GAME;
-    		setFocusable(false);
-    		BH.game.requestFocus();
-    	}
     }
 	    
 }
