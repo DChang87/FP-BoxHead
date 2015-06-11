0ebb8cd02cfd44f0d86395cea59e0e7b64ef3d1c
Diana Chang <diana.chang.twn@gmail.com>
2015-06-11 15:00:40 -0400

    06/11/2015 3:00pm

--- MagicalBox.java
+++ MagicalBox.java
@@ -7,7 +7,7 @@ class MagicalBox {
 	private final int ITEMCOUNT=10, bsx = 28, bsy = 28;
 	private int counter=200;
 	//stop
-	
+	private String[] names = new String[ITEMCOUNT]; 
 	BoxHead BH;
 	public MagicalBox(int x, int y,BoxHead bh){
 		X=x;
@@ -29,19 +29,27 @@ class MagicalBox {
 	public int generateItem(){
 		int[] allItems = new int[ITEMCOUNT];
 		allItems[0]=0;//health
+		names[0]="health";
 		allItems[1]=2;//UZI
+		names[1]="UZI";
 		//no need for pistol since pistol is unlimited
 		allItems[2]=3; //shotgun		
+		names[2]="Shotgun";
 		allItems[3]=4; //barrels
+		names[3]="Barrels";
 		allItems[4]=5;//grenade
+		names[4]="Grenade";
 		allItems[5]=6;//fake walls
+		names[5]="Barricades";
 		allItems[6]=7;//sentry gun
+		names[6]="Sentry Gun";
 		
 		
 		
 		ITEM=allItems[(int) (Math.random()*(BH.magicalBoxAllowance-1))];
 		//n is passed in as the number of the weapon which accounts for pistol
 		//pistol is not accounted here so it must be taken away
+		BH.game.setBoxString(names[ITEM]);
 		return ITEM;
 	}
 	public int getX(){
