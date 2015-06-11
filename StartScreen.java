8cd3048f2449a711d3addd3bbe422b04bacbff07
Diana Chang <diana.chang.twn@gmail.com>
2015-06-11 18:35:49 -0400

    06/11/2015 6:35pm button fixed

--- StartScreen.java
+++ StartScreen.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -12,9 +14,10 @@ public class StartScreen extends JPanel implements MouseMotionListener, MouseLis
 	private Image up = new ImageIcon("startup.jpg").getImage();
 	private Image hover = new ImageIcon("starthover.jpg").getImage();
 	private Image down = new ImageIcon("startdown.jpg").getImage();
-	private int bx=350,by=400,bLength=50,bWidth=100;
+	private int bx=220,by=350,bLength=55,bWidth=390;
 	private boolean Down=false;
 	BoxHead BH;
+	private Font font = new Font("Century Gothic", Font.PLAIN, 70);
 	private int counter=50;
 	public StartScreen(BoxHead b){
 		//load the images for the start button
@@ -28,28 +31,12 @@ public class StartScreen extends JPanel implements MouseMotionListener, MouseLis
     	//requestFocus();
     //}
     
-	public void mouseClicked(MouseEvent e) {
-		
-	}
-	
-	public void mouseEntered(MouseEvent e) {
-		
-	}
-	
-	public void mouseExited(MouseEvent e) {
-		
-	}
-	
-	public void mousePressed(MouseEvent e) {
-		Down=true;
-	}
-
-	public void mouseReleased(MouseEvent e) {
-		Down=false;
-	}
-	public void mouseDragged(MouseEvent e) {
-		
-	}
+	public void mouseClicked(MouseEvent e) {}	
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {Down=true;}
+	public void mouseReleased(MouseEvent e) {Down=false;}
+	public void mouseDragged(MouseEvent e) {}
 	public void mouseMoved(MouseEvent e) {
 		mouseX = e.getX();
 		mouseY = e.getY();
@@ -58,17 +45,25 @@ public class StartScreen extends JPanel implements MouseMotionListener, MouseLis
 		return bx<=mouseX&&bx+bWidth>=mouseX&&by<=mouseY&&by+bLength>=mouseY;
 	}
 	public void paintComponent(Graphics g){
+		g.setFont(font);
     	g.drawImage(background,0,0,this);
     	if (collide()&&Down){
-    		g.drawImage(down, bx, by, this);
+    		//g.drawImage(down, bx, by, this);
+    		g.setColor(Color.black);
+    		g.drawString("Start Game", bx, by+bLength);
     		BH.state=BH.GAME;
     		BH.game.requestFocus();
     	}
     	else if (collide()){
-    		g.drawImage(hover,bx,by,this);
+    		//g.drawImage(hover,bx,by,this);
+    		g.setColor(Color.gray);
+    		g.drawString("Start Game", bx, by+bLength);
     	}
     	else{
-    		g.drawImage(up,bx,by,this);
+    		//g.drawImage(up,bx,by,this);
+    		//g.drawRect(bx, by, bWidth, bLength);
+    		g.setColor(Color.black);
+    		g.drawString("Start Game", bx, by+bLength);
     	}
     }
 	    
