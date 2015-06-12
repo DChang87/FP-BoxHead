2895c2fceea95b1d77f4d7072622633727bbc9fb
Diana Chang <diana.chang.twn@gmail.com>
2015-06-12 01:59:23 -0400

    commented

--- StartScreen.java
+++ StartScreen.java
@@ -11,26 +11,16 @@ import javax.swing.JPanel;
 public class StartScreen extends JPanel implements MouseMotionListener, MouseListener{
 	private static int mouseX,mouseY;
 	private Image background = new ImageIcon("tempbackground.jpg").getImage();
-	private Image up = new ImageIcon("startup.jpg").getImage();
-	private Image hover = new ImageIcon("starthover.jpg").getImage();
-	private Image down = new ImageIcon("startdown.jpg").getImage();
-	private int bx=240,by=350,bLength=57,bWidth=338;
-	private boolean Down=false;
+	private int bx=240,by=350,bLength=57,bWidth=338; //dimensions for the "START GAME" string
+	private boolean Down=false; //if the mouse is down
 	BoxHead BH;
 	private Font font = new Font("Impact", Font.PLAIN, 70);
-	private int counter=50;
 	public StartScreen(BoxHead b){
-		//load the images for the start button
 		BH=b;
 		setSize(800,640);
 		addMouseMotionListener(this);
 		addMouseListener(this);
 	}
-    //public void addNotify() {
-    	//super.addNotify();
-    	//requestFocus();
-    //}
-    
 	public void mouseClicked(MouseEvent e) {}	
 	public void mouseEntered(MouseEvent e) {}
 	public void mouseExited(MouseEvent e) {}
@@ -42,26 +32,27 @@ public class StartScreen extends JPanel implements MouseMotionListener, MouseLis
 		mouseY = e.getY();
 	}
 	public boolean collide(){
+		//if mouse collides with the rectangle containing the string
 		return bx<=mouseX&&bx+bWidth>=mouseX&&by<=mouseY&&by+bLength>=mouseY;
 	}
 	public void paintComponent(Graphics g){
 		g.setFont(font);
     	g.drawImage(background,0,0,this);
+    	
+    	//ny+nLength instead of ny so the String would fit perfectly in the rectangle for collision
     	if (collide()&&Down){
-    		//g.drawImage(down, bx, by, this);
+    		//pressed
     		g.setColor(Color.black);
     		g.drawString("START GAME", bx, by+bLength);
     		BH.state=BH.GAME;
     		BH.game.requestFocus();
     	}
     	else if (collide()){
-    		//g.drawImage(hover,bx,by,this);
+    		//hovering
     		g.setColor(Color.gray);
     		g.drawString("START GAME", bx, by+bLength);
     	}
     	else{
-    		//g.drawImage(up,bx,by,this);
-    		//g.drawRect(bx, by, bWidth, bLength);
     		g.setColor(Color.black);
     		g.drawString("START GAME", bx, by+bLength);
     	}
