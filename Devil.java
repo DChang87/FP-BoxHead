db651093a6450f21f9757c26384cef26f2813c23
Leo F. <leofeng1999@gmail.com>
2015-06-12 00:43:11 -0400

    ASDJKOADJO :DDD

--- Devil.java
+++ Devil.java
@@ -1,5 +1,8 @@
 import java.util.*;
 class Devil {
+	//I like to call it the red zombie
+	//shoots fireballs at the MC
+	//fireballs do massive damage
 	private int Health = 100;
 	private int angle; //angle is the angle in which the character is facing
 	//-> is 0
@@ -8,9 +11,9 @@ class Devil {
 	private double posx, posy;
 	private int sx=34,sy=47;
 	private int timeCount=0; //counter to see when to shoot a fireball
-	private int sp=2, spriteCounter=0;
+	private int sp=2, spriteCounter=0;//sp is speed
 	MainCharacter mc;
-	private final int timeCountLim = 100;
+	private final int timeCountLim = 100;//when reaches shoots fireball
 	public Devil(int x, int y, int ang, MainCharacter mccc){
 		posx = x;
 		posy = y;
@@ -65,11 +68,6 @@ class Devil {
 	public void setY(double y){
 		posy = y;
 	}
-	public void inContact(MainCharacter Leo){
-		if (Leo.getX()==posx && Leo.getY()==posy){
-			Leo.setHealth(Leo.getHealth()-5);
-		}
-	}
 	public int getHealth(){
 		return Health;
 	}
@@ -83,7 +81,7 @@ class Devil {
 	}
 	public boolean getCollide(int x, int y){
 		return (x >= posx && x <= posx+sx && y >= posy && y <= posy+sy);
-	}
+	}//collision with a point
 	
 	public int getsx(){
 		return sx;
@@ -94,8 +92,6 @@ class Devil {
 	public void addTime(BoxHead BH){
 		//call this method during the main method with a timer to call it at regular intervals
 		timeCount++;
-		//System.out.println(angle);
-		//System.out.println(timeCount+ " " + timeCountLim);
 		if (timeCount==timeCountLim){
 			timeCount=0;
 			BH.game.fireballs.add(new PosPair((int)posx,(int)posy,angle,FIREBALL)); 
