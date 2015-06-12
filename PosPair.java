a36d44405112233a13dbfff9a9e634d1c916614e
Diana Chang <diana.chang.twn@gmail.com>
2015-06-12 10:39:18 -0400

    updated 10:39am

--- PosPair.java
+++ PosPair.java
@@ -2,7 +2,7 @@ public class PosPair{
 	//The place where fireballs, bullets and very dangerous weapons are stored
     private final int ANGLE, Type, dmg = 350;
     private double X,Y, origX, origY;
-    //				x, 		y,	degree bullet/fireball will travel
+    //			   x,y,	original position of the bullet, degree bullet/fireball will travel
     public PosPair(int x,int y,int deg,int type)//type is type of bullet or fireball
     {
         X=x;
@@ -12,36 +12,18 @@ public class PosPair{
         ANGLE = deg;
         Type = type;
     }
-    public int getX(){
-    	return (int)X;
-    }
-    public int getY(){
-    	return (int)Y;
-    }
-    public int getdmg(){
-    	return dmg;
-    }
-    public int getType(){
-    	return Type;
-    }
-    public int getorigX(){
-    	return (int)origX;
-    }
-    public int getorigY(){
-    	return (int)origY;
-    }
-    public double getDX(){
-    	return X;
-    }
-    public double getDY(){
-    	return Y;
-    }
-    public int getANGLE(){
-    	return ANGLE;
-    }
-    public int getTYPE(){
-    	return Type;
-    }
+    
+    //////////getter/setters/////////////////
+    public int getX(){return (int)X;}
+    public int getY(){return (int)Y;}
+    public int getdmg(){return dmg;}
+    public int getType(){return Type;}
+    public int getorigX(){return (int)origX;}
+    public int getorigY(){return (int)origY;}
+    public double getDX(){return X;}
+    public double getDY(){return Y;}
+    public int getANGLE(){return ANGLE;}
+    public int getTYPE(){return Type;} //type of item flying and moving (Ex. fireball, bullets, etc.)
     public void setPos(double x, double y){
     	X = x;
     	Y = y;
