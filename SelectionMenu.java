07711f5a59c808bdc49b6048b2982d556f07ae7e
Diana Chang <diana.chang.twn@gmail.com>
2015-06-12 01:47:16 -0400

    COMMENTED

--- SelectionMenu.java
+++ SelectionMenu.java
@@ -3,52 +3,40 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 public class SelectionMenu extends JPanel implements KeyListener{
-	private static int mouseX,mouseY;
 	private Image background = new ImageIcon("selectionBackground.jpg").getImage();
-	private boolean down=false;
 	BoxHead BH;
 	private boolean[] keys;
 	private Font font = new Font("Impact", Font.PLAIN, 35);
 	public SelectionMenu(BoxHead b){
-		//load the images for the start button
-		System.out.println("Selection menu");
 		keys = new boolean[65535];
 		BH=b;
 		addKeyListener(this);
 		setSize(800,640);
 	}
-    //public void addNotify() {
-    	//super.addNotify();
-    	//requestFocus();
-    //}
-    public void keyTyped(KeyEvent e){
-		
-	}
-    public void keyPressed(KeyEvent e){
-    	keys[e.getKeyCode()]=true;
-    }
-    public void keyReleased(KeyEvent e){
-    	keys[e.getKeyCode()]=false;
-    }
+    public void keyTyped(KeyEvent e){}
+    public void keyPressed(KeyEvent e){keys[e.getKeyCode()]=true;}
+    public void keyReleased(KeyEvent e){keys[e.getKeyCode()]=false;}
     public void checkUnPause(){
-    	if (keys[KeyEvent.VK_P] || down){
+    	//check if the user decided to return to the game
+    	if (keys[KeyEvent.VK_P]){
 			BH.state=BH.GAME;
-			System.out.println("checkunpause");
 			BH.game.requestFocus();
-			keys[KeyEvent.VK_P]=false;
+			keys[KeyEvent.VK_P]=false; //set to false so next time selectionMenu is needed, keys[KeyEvent.VK_P] would not be set at true and disrupt the program
 		}
 	}
     public void paintComponent(Graphics g){
     	g.setFont(font);
     	g.drawImage(background,0,0,this);
+    	
+    	//draw the names of all the available upgrades
+    	//if they have been obtained, they are in green
+    	//if not then they are in read
+    	//half on one side half on the other side of the screen
     	for (int i=0;i<13;i++){
     		if (BH.game.getNextUpgrade()>i){
     			g.setColor(Color.green);
